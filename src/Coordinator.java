a8319da7970ea9ea417ae279f76a4375e05f7e6a
kamilfocus <k4mil.piszczek@gmail.com>
2015-06-01 15:28:55 +0200

    Koordynator - szkielet

---
+++ src/Coordinator.java
@@ -0,0 +1,72 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by KAMIL on 2015-06-01.
+ */
+public class Coordinator {
+
+    List<Activity> activities;
+    Integer currentSimulationTime;
+    Integer totalSimulationTime;
+    Boolean allProceduresFinished;
+
+    public class Activity{
+        String header;
+        Integer stateChangeTime;
+
+        Activity(String header, Integer stateChangeTime){
+            this.header = header;
+            this.stateChangeTime = stateChangeTime;
+        }
+
+        Integer getStateChangeTime(){
+            return stateChangeTime;
+        }
+    }
+
+    Coordinator(Integer totalSimulationTime){
+        this.currentSimulationTime = new Integer(0);
+        this.totalSimulationTime = totalSimulationTime;
+        allProceduresFinished = new Boolean(false);
+
+        generateInitialActivites();
+    }
+
+    private void generateInitialActivites(){
+        activities = new LinkedList<Activity>();
+        //@TODO Wygenerowanie listy dzia³an
+    }
+
+    void startSimulation(){
+
+        allProceduresFinished = activities.isEmpty();
+
+        while(allProceduresFinished == false && currentSimulationTime<totalSimulationTime){
+            currentSimulationTime = scanTime();
+            scanActivities();
+            allProceduresFinished = activities.isEmpty();
+        }
+    }
+
+    private Integer scanTime(){
+        Integer minActivityTime = activities.get(0).getStateChangeTime();
+        for(Activity activity : activities)
+            if(minActivityTime > activity.getStateChangeTime())
+                minActivityTime = activity.getStateChangeTime();
+        return minActivityTime;
+    }
+
+    private void scanActivities(){
+        Iterator <Activity> activityIterator = activities.iterator();
+        while(activityIterator.hasNext()){
+            Activity currentActivity = activityIterator.next();
+            if(currentActivity.getStateChangeTime() == currentSimulationTime){
+                //@TODO Sprawdz warunki unikalne dla kazdego headera (Boolean checkConditions(String header)) i uruchom procedure (startProcedure(String header)
+                activityIterator.remove();
+            }
+        }
+    }
+
+}
--- src/Simulation.java
+++ src/Simulation.java
@@ -2,7 +2,11 @@
  * Created by KAMIL on 2015-06-01.
  */
 public class Simulation {
+
     static public void main(String [] args){
         System.out.println("Hello Didi !");
+        Integer totalSimulationTime = new Integer(1000);
+        Coordinator coordinator = new Coordinator(totalSimulationTime);
+        coordinator.startSimulation();
     }
 }
